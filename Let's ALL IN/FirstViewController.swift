3c8e40233ee9aaa7754d5aa95999988e334149b8
RayHsiao <kevin1002045@gmail.com>
2024-04-11 15:48:52 +0800

    第一次紀錄

--- Let's ALL IN/FirstViewController.swift
+++ Let's ALL IN/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  Let's ALL IN
 //
 //  Created by 蕭煜勳 on 2024/4/11.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FirstViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
+    
+    
 }
-
